890049b65146d2ea77c3b0c6fa26e5141ccc3846
Fernanda <fefecoelho0508@gmail.com>
2023-03-14 19:14:39 -0400

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PesoIdeal
+{
+    public partial class CalculaPesoIdeal : Form
+
+    {
+        RadioButton rbnSelecionado = null;
+
+        public CalculaPesoIdeal()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculaPesoIdeal_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+        private void SetPesoIdeal()
+        {
+            try
+            {
+                
+                double altura = Convert.ToDouble(txtAltura.Text);
+                double pesoIdeal;
+                if (rbnSelecionado.Text.Equals("Masculino"))
+                    pesoIdeal = (72.7 * altura) - 58;
+                else
+                    pesoIdeal = (62.1 * altura) - 44.7;
+                lblPesoIdeal.Text = pesoIdeal.ToString("N");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Selecione o sexo e informe a altura corretamente", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void rbtnMasc_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rbn = (RadioButton)sender;
+            if (rbn.Checked)
+            {
+                rbnSelecionado = rbn;
+                SetPesoIdeal();
+            }
+        }
+
+        private void txtAltura_TextChanged(object sender, EventArgs e)
+        {
+            SetPesoIdeal();
+
+        }
+
+        private void rbtnFem_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rbn = (RadioButton)sender;
+            if (rbn.Checked)
+            {
+                rbnSelecionado = rbn;
+                SetPesoIdeal();
+            }
+
+        }
+    }
+}
